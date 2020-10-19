66096b9eb1e820641eab0915b6f1163c2737208d
Furkan AKGUN <fakgun@yukselproje.com.tr>
2020-10-19 15:32:02 +0300

    first-commit

---
+++ lib/pages/search.dart
@@ -0,0 +1,160 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:fluttershare/models/user.dart';
+import 'package:fluttershare/pages/home.dart';
+import 'package:fluttershare/widgets/progress.dart';
+
+class Search extends StatefulWidget {
+  @override
+  _SearchState createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  TextEditingController searchController = TextEditingController();
+  Future<QuerySnapshot> searchResultsFuture;
+
+  handleSearch(String query) {
+    Future<QuerySnapshot> users = usersRef
+        .where("username", isGreaterThanOrEqualTo: query)
+        .where("username", isLessThan: query + 'z')
+        .getDocuments();
+    setState(() {
+      searchResultsFuture = users;
+    });
+  }
+
+  clearSearch() {
+    searchController.clear();
+    clearSearchList();
+  }
+
+  clearSearchList() {
+    searchResultsFuture = null;
+  }
+
+  AppBar buildSearchField() {
+    return AppBar(
+      backgroundColor: Colors.grey[900],
+      title: TextFormField(
+        style: TextStyle(color: Colors.white),
+        controller: searchController,
+        decoration: InputDecoration(
+          hintText: "Search for a user...",
+          hintStyle: TextStyle(color: Colors.white),
+          filled: true,
+
+          // prefixIcon: Icon(
+          //   Icons.account_box,
+          //   size: 28.0,
+          //   color: Colors.white,
+          // ),
+          suffixIcon: IconButton(
+            color: Colors.white,
+            icon: Icon(Icons.clear),
+            onPressed: clearSearch,
+          ),
+        ),
+        onChanged: handleSearch,
+        onFieldSubmitted: handleSearch,
+      ),
+    );
+  }
+
+  Container buildNoContent() {
+    final Orientation orientation = MediaQuery.of(context).orientation;
+    return Container(
+      child: Center(
+        child: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            // SvgPicture.asset(
+            //   'assets/images/search.svg',
+            //   height: orientation == Orientation.portrait ? 300.0 : 200.0,
+            // ),
+            // Text(
+            //   "Find Users",
+            //   textAlign: TextAlign.center,
+            //   style: TextStyle(
+            //     color: Colors.white,
+            //     fontStyle: FontStyle.italic,
+            //     fontWeight: FontWeight.w600,
+            //     fontSize: 60.0,
+            //   ),
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  buildSearchResults() {
+    return FutureBuilder(
+      future: searchResultsFuture,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return circularProgress();
+        }
+        List<UserResult> searchResults = [];
+        snapshot.data.documents.forEach((doc) {
+          User user = User.fromDocument(doc);
+          UserResult searchResult = UserResult(user);
+          searchResults.add(searchResult);
+        });
+        return ListView(
+          children: searchResults,
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: buildSearchField(),
+      body:
+          searchResultsFuture == null ? buildNoContent() : buildSearchResults(),
+    );
+  }
+}
+
+class UserResult extends StatelessWidget {
+  final User user;
+
+  UserResult(this.user);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: Column(
+        children: <Widget>[
+          GestureDetector(
+            onTap: () => print('tapped'),
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.black,
+                backgroundImage: CachedNetworkImageProvider(user.photoUrl),
+              ),
+              title: Text(
+                user.displayName,
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                user.username,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          Divider(
+            height: 2.0,
+            color: Colors.white54,
+          ),
+        ],
+      ),
+    );
+  }
+}
